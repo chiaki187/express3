12be120af10177f05b959bb6a98409705f158895
chiaki187 <23fi094@ms.dendai.ac.jp>
2025-07-06 10:32:13 +0900

    main =mainchiakiに変更

--- public/main.js
+++ public/main.js
@@ -1,49 +1,41 @@
-// WebSocketサーバーに接続
-const ws = new WebSocket('ws://' + window.location.host + '/ws');
+const express = require('express')
+const expressWs = require('express-ws')
 
-ws.onmessage = function(event) {
-  // サーバーから"done"メッセージを受信したら画面遷移
-  if (event.data === "done") {
-    window.location.href = "index.html";
-  }
-};
+const app = express()
+expressWs(app)
 
-// 通信がcloseした場合にも画面遷移したい場合はこちらを有効化
-  ws.onclose = function() {
-  window.location.href = "connect.html";
-  };
+const port = process.env.PORT || 3001
+let connects = []
 
-  const express = require('express')
-  const expressWs = require('express-ws')
-  
-  const app = express()
-  expressWs(app)
-  
-  const port = process.env.PORT || 3001
-  let connects = []
-  
-  app.use(express.static('public'))
-  
-  app.ws('/ws', (ws, req) => {　// WebSocket endpoint
-   
-    connects.push(ws)
-  
-    ws.on('message', (message) => {
-      console.log('Received:', message)
-  
-      connects.forEach((socket) => {
-        if (socket.readyState === 1) {
-          // Check if the connection is open
-          socket.send(message)
-        }
-      })
+app.use(express.static('public'))
+
+app.ws('/ws', (ws, req) => {
+  connects.push(ws)
+
+  if (connects.length === 2) {
+    connects.forEach((socket) => {
+      if (socket.readyState === 1) {
+        socket.send(JSON.stringify({ type: 'ready', text: '接続完了' }))
+      }
     })
-  
-    ws.on('close', () => {
-      connects = connects.filter((conn) => conn !== ws)
+  }
+
+  ws.on('message', (message) => {
+    console.log('Received:', message)
+
+    connects.forEach((socket) => {
+      if (socket.readyState === 1) {
+        // Check if the connection is open
+        socket.send(message)
+      }
     })
   })
-  
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-  }) 
+
+  ws.on('close', () => {
+    connects = connects.filter((conn) => conn !== ws)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
