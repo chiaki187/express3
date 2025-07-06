87e642a50b8b8d37797dbd0e1da2e0ac442dc5ee
kamome257 <23fi051@ms.dendai.ac.jp>
2025-07-06 10:07:58 +0900

    addapptomain

--- public/main.js
+++ public/main.js
@@ -12,3 +12,38 @@ ws.onmessage = function(event) {
   ws.onclose = function() {
   window.location.href = "connect.html";
   };
+
+  const express = require('express')
+  const expressWs = require('express-ws')
+  
+  const app = express()
+  expressWs(app)
+  
+  const port = process.env.PORT || 3001
+  let connects = []
+  
+  app.use(express.static('public'))
+  
+  app.ws('/ws', (ws, req) => {　// WebSocket endpoint
+   
+    connects.push(ws)
+  
+    ws.on('message', (message) => {
+      console.log('Received:', message)
+  
+      connects.forEach((socket) => {
+        if (socket.readyState === 1) {
+          // Check if the connection is open
+          socket.send(message)
+        }
+      })
+    })
+  
+    ws.on('close', () => {
+      connects = connects.filter((conn) => conn !== ws)
+    })
+  })
+  
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+  }) 
