bf38f23bab70385abb107d21cf8fe5aac79338ac
kamome257 <23fi051@ms.dendai.ac.jp>
2025-07-06 11:22:23 +0900

    connnectTest

--- public/main.js
+++ public/main.js
@@ -1,49 +1,19 @@
-// WebSocketサーバーに接続
-const ws = new WebSocket('ws://' + window.location.host + '/ws');
+// ページを開いたときにconnect.htmlでなければ自動でconnect.htmlに遷移
+if (window.location.pathname !== '/connect.html') {
+  window.location.href = 'connect.html';
+} else {
+  // WebSocketサーバーに接続
+  const ws = new WebSocket('ws://' + window.location.host + '/ws');
 
-ws.onmessage = function(event) {
-  // サーバーから"done"メッセージを受信したら画面遷移
-  if (event.data === "done") {
-    window.location.href = "index.html";
-  }
-};
-
-// 通信がcloseした場合にも画面遷移したい場合はこちらを有効化
-  ws.onclose = function() {
-  window.location.href = "connect.html";
+  ws.onmessage = function(event) {
+    // サーバーから"done"メッセージを受信したら画面遷移
+    if (event.data === "done") {
+      window.location.href = "index.html";
+    }
   };
 
-  const express = require('express')
-  const expressWs = require('express-ws')
-  
-  const app = express()
-  expressWs(app)
-  
-  const port = process.env.PORT || 3001
-  let connects = []
-  
-  app.use(express.static('public'))
-  
-  app.ws('/ws', (ws, req) => {　// WebSocket endpoint
-   
-    connects.push(ws)
-  
-    ws.on('message', (message) => {
-      console.log('Received:', message)
-  
-      connects.forEach((socket) => {
-        if (socket.readyState === 1) {
-          // Check if the connection is open
-          socket.send(message)
-        }
-      })
-    })
-  
-    ws.on('close', () => {
-      connects = connects.filter((conn) => conn !== ws)
-    })
-  })
-  
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-  }) 
+  ws.onclose = function() {
+    // 接続が解除されたらconnect.htmlへ遷移
+    window.location.href = 'connect.html';
+  };
+}
