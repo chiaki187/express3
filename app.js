3ea55a69f4534e3c7871c3ffd78a90fbbde77909
kamome257 <23fi051@ms.dendai.ac.jp>
2025-07-11 11:49:57 +0900

    back

--- app.js
+++ app.js
@@ -55,14 +55,9 @@ app.listen(port, () => {
   console.log(`Server is running on https://localhost:${port}`)
 })
 
- /*setInterval(() => {
+ setInterval(() => {
   console.log(`[WebSocket] 現在の接続数: ${connects.length}`);
-}, 10000); // 10秒ごと*/
+}, 10000); // 10秒ごと
 
-  app.getConnections((error, count) => {
-    if (error) {
-      console.error('Error getting connections:', error);
-    } else {
-      console.log('Number of current connections:', count);
-    }
-  }, 10000);
+
+  
