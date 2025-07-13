81927b8aa41250a473fc4117142048416f56e40d
kamome257 <23fi051@ms.dendai.ac.jp>
2025-07-13 12:11:26 +0900

    main

--- public/main.js
+++ public/main.js
@@ -5,6 +5,20 @@ const app = express()
 expressWs(app)
 
 
+app.use(express.static('public'))
+
+app.ws('/ws', (ws, req) => {
+  connects.push(ws)
+ // 2人接続したら、ゲーム開始準備の信号を送る
+  if (connects.length === 2) {
+    console.log('サーバー: 2人接続しました。ready信号を送ります。');
+    connects.forEach((socket) => {
+      if (socket.readyState === 1) {
+        console.log('サーバー：ready信号を送ったよ。');
+        socket.send(JSON.stringify({ type: 'ready', text: '接続完了' }))
+      }
+    })
+}
 
 if (data.type === 'user_ready') { //ユーザーがゲーム開始ボタンを押したら
   if (!start.includes(socket)) {
