4d37eb5d8b1ad6009f6c086d2bb5bfe1d9ce4a02
chiaki187 <23fi094@ms.dendai.ac.jp>
2025-07-16 11:50:48 +0900

    html

--- public/app.js
+++ public/app.js
@@ -109,8 +109,6 @@ function initializeCharacterPositions() {
     // ゲームオーバー/クリア状態をリセット
     isGameOver = false;
     isGameClear = false;
-    // 背景色もリセット
-    gameFrameBlack.style.backgroundColor = '#8484ff'; 
     // 速度もリセット
     moveSpeed = 10;
     enemySpeed1 = 1; // initializeCharacterPositions内で速度をリセット
