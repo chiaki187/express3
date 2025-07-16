8e6a6af61dbc2421a3eb2427e680973c20011580
chiaki187 <23fi094@ms.dendai.ac.jp>
2025-07-16 11:42:39 +0900

    html

--- public/app.js
+++ public/app.js
@@ -1,3 +1,21 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// 次/前のコントロール
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
 // ゲームの状態を管理するフラグ
 let isGameOver = false;
 let isGameClear = false; // ゲームクリア状態を管理するフラグ
